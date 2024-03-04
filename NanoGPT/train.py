82b6e5321085cd58be3a4db818a71cc869c15695
NastyTester <70013669+AlinNast@users.noreply.github.com>
2024-03-04 12:20:04 +0200

    Added a Basic Bigram model

--- NanoGPT/train.py
+++ NanoGPT/train.py
@@ -1,4 +1,6 @@
 import torch
+import torch.nn as nn
+from torch.nn import functional as F
 
 
 ## This opens the training data. 
@@ -37,3 +39,33 @@ def get_batch(split):
     return x, y
 
 xb, yb = get_batch('train')
+
+
+# super simple bigram model, already built by torch.nn module
+class BigramLanguageModel(nn.Module):
+
+    def __init__(self, vocab_size):
+        super().__init__()
+        # each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets=None):
+
+        # idx and targets are both (B,T) tensor of integers
+        logits = self.token_embedding_table(idx) # (B,T,C)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+    
+    
+m = BigramLanguageModel(vocab_size)
+out = m(xb, yb)
+print(out.shape)
+
